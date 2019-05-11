cfc6b2a26130c0fbbfb1b59788173a346859f4cc
Sergio M <38441398+Sergiotmg@users.noreply.github.com>
2019-05-11 14:55:09 +0200

    Initial commit REST API con Spring y Gradle

---
+++ src/main/java/exception/PacienteNotFoundException.java
@@ -0,0 +1,8 @@
+package exception;
+
+public class PacienteNotFoundException extends Exception {
+    private long id;
+    public PacienteNotFoundException(Long id) {
+        super(String.format("Paciente con id : '%s' no encontrado", id));
+    }
+}
